0cb42ee8300276cc6390aeeca36a0a89028a5f3d
Ociel Lerma <oglerma@gmail.com>
2019-03-26 21:47:44 -0700

    Adding a UIAlertController that gives the user the option to navigate to a different site, but it is not finsihed yet.

--- WebBrowserApp/WebBrowserApp/ViewController.swift
+++ WebBrowserApp/WebBrowserApp/ViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
+    var openImage: UIImage?
     
     override func loadView(){
         webView = WKWebView()
@@ -26,6 +27,27 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        openImage = UIImage(named: "checkmark.png")
+    
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+    }
+    
+    func loadImage()-> UIImage?{
+        let image = UIImage(named: "chekcmark.png")
+        
+        return image
+    }
+    
+    @objc
+    func openTapped(){
+//        let vc = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+//        vc.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+//        vc.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+    }
+    
+    func openPage(){
         
     }
 
