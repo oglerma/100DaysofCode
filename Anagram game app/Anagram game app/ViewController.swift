719a4e082e84f68188438b0a4b3b6331f7fb15a1
Ociel Lerma <oglerma@gmail.com>
2019-04-02 07:50:42 -0700

    Anagram game app.
    Imported a file called start.txt with a lot of 8 character words in which we will use throughout the project.

---
+++ Anagram game app/Anagram game app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anagram game app
+//
+//  Created by Ociel Lerma on 4/2/19.
+//  Copyright © 2019 Ociel Lerma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
