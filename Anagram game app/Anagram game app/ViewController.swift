10b13d157f2f548709e6151170caf72a513a92fa
Ociel Lerma <oglerma@gmail.com>
2019-04-02 08:02:48 -0700

    Added a load function inside viewDidLoad() which looks for a text file called start in our project using the Bundle.main.url method. We then store the info inside that file into an array. each word inside the file is seperated by a "\n" so we call the components function and seperate each word by that character. If we don't find it then we give it at least one word, in this case we give it "silkworm"

--- Anagram game app/Anagram game app/ViewController.swift
+++ Anagram game app/Anagram game app/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadWords()
+        
+    }
+    
+    // Search for txt file in project and load content into an array
+    func loadWords(){
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsURL){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
     }
 
+    
 
 }
 
