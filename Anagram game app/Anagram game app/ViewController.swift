42d0815af3dcf8c63eb3a516a86ac402b7314d39
Ociel Lerma <oglerma@gmail.com>
2019-04-02 08:07:56 -0700

    Added a start game function and gave the table view its numberofRowsInSection and its cellFOrRowPath

--- Anagram game app/Anagram game app/ViewController.swift
+++ Anagram game app/Anagram game app/ViewController.swift
@@ -31,7 +31,24 @@ class ViewController: UITableViewController {
         }
     }
 
+    func startGame(){
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        
+        // refreshes the current tableview controller
+        tableView.reloadData()
+    }
     
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 
 }
 
