43052d0daa1fad08ba60f335787d9ef489171452
Ociel Lerma <oglerma@gmail.com>
2019-04-02 21:25:40 -0700

    Added a right bar navigation item that when pressed it presents an UIAlertViewController that can take an input. I use that string that was just inputed and i add it to the top of the tableview by using .insertRows() function provided by tableview.

--- Anagram game app/Anagram game app/ViewController.swift
+++ Anagram game app/Anagram game app/ViewController.swift
@@ -15,7 +15,12 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(promptForAnswer))
         loadWords()
+        startGame()
         
     }
     
@@ -49,6 +54,57 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
+    
+    @objc func promptForAnswer(){
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        // Using Trailing closures
+        let submitAction = UIAlertAction(title: "Submit", style: .default){
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+            
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    func submit(_ answer: String){
+        let lowerAnswer = answer.lowercased()
+        if isPossible(word: lowerAnswer){
+            if isOriginal(word: lowerAnswer){
+                if isReal(word: lowerAnswer){
+                    //Inserts word at the top and if there is a word
+                    //already in then it scoots it to the next index.
+                    usedWords.insert(lowerAnswer, at: 0)
+                    
+                    //Gets indexPath.row and give it a Zero so when we call
+                    //the insertRows we can give it the first index and it
+                    //will animate this. Another way around if we don't want
+                    //to reload the entire project.
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+        
+    }
+    
+    func isPossible(word: String) -> Bool{
+        return true
+    }
+    
+    func isOriginal(word: String)-> Bool{
+        return true
+    }
+    
+    func isReal(word: String) -> Bool{
+        return true
+    }
+    
+    
 
 }
 
