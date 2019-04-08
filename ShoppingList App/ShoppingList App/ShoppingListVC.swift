090147130685da831e7f1d77fe1222800bcd81a7
Ociel Lerma <olerma@ebay.com>
2019-04-08 09:20:28 -0700

    Added functionality to the functions we have created. It is not showing anything so a few things need to change to show.

--- ShoppingList App/ShoppingList App/ShoppingListVC.swift
+++ ShoppingList App/ShoppingList App/ShoppingListVC.swift
@@ -15,31 +15,44 @@ class ShoppingListVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Add the Add an Item button
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(askForItem))
         // TODO: Add Alert ViewController that takes in text
         // TODO: Display Text From AlertVC into ShoppingListTableView
         // TODO: Add a Function to start a new list
         // TODO: Add a title in the Navigation bar
         title = "Shopping List"
         // TODO: Add a delete swipe
+        makeNewList()
     }
 
-    @objc func addItem(){
-        getUserInput()
+   @objc func askForItem(){
         
+        let ac = UIAlertController(title: "Add and Item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Add", style: .default) {
+            //TODO: Study this more
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else {return}
+            self?.submit(answer)
+        })
+            
+        present(ac, animated: true)
     }
     
-    func getUserInput(){
-        let ac = UIAlertController(title: "Add and Item", message: nil, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action) in
-            print("Do something here")
-            }))
+    func submit(_ answer: String){
+        shoppingListArray.insert(answer, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
     
     func makeNewList(){
-        
+        shoppingListArray.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
     
+    
+    
+    // Rows and
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingListArray.count
     }
