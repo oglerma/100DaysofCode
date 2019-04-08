690fd1c0c7435e7529defd80b21c4f40ed07f772
Ociel Lerma <oglerma@gmail.com>
2019-04-08 08:03:07 -0700

    Added: NavigationItem, title to NavigationBar, and three of the TODO functions are named but not yet usable.

--- ShoppingList App/ShoppingList App/ShoppingListVC.swift
+++ ShoppingList App/ShoppingList App/ShoppingListVC.swift
@@ -15,18 +15,35 @@ class ShoppingListVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Add the Add an Item button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
         // TODO: Add Alert ViewController that takes in text
         // TODO: Display Text From AlertVC into ShoppingListTableView
         // TODO: Add a Function to start a new list
         // TODO: Add a title in the Navigation bar
+        title = "Shopping List"
         // TODO: Add a delete swipe
     }
 
+    @objc func addItem(){
+        getUserInput()
+        
+    }
+    
+    func getUserInput(){
+        let ac = UIAlertController(title: "Add and Item", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action) in
+            print("Do something here")
+            }))
+    }
+    
+    func makeNewList(){
+        
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingListArray.count
     }
-
- 
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingCell", for: indexPath)
         cell.textLabel?.text = shoppingListArray[indexPath.row]
