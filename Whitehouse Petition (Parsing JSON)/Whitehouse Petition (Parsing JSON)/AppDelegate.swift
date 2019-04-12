d22985badee081dc8581c54f11e2d5b3b722302f
Ociel Lerma <oglerma@gmail.com>
2019-04-12 07:50:44 -0700

    New bar item was created in the Appdelegate file programmatically. Error alert was also created in the case that data is not parsed correctly from JSON.

--- Whitehouse Petition (Parsing JSON)/Whitehouse Petition (Parsing JSON)/AppDelegate.swift
+++ Whitehouse Petition (Parsing JSON)/Whitehouse Petition (Parsing JSON)/AppDelegate.swift
@@ -15,32 +15,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Adding a new tabBarItem to our current RootViewControlles (Steps)
+        
+        // Check to see if there is a rootViewController with a subclass of UITabBarController
+        if let tabBarViewController = window?.rootViewController as? UITabBarController{
+            
+            // IF there is one, find it using the name given to it. (i.e. Main.storyboard)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            // Crete a new ViewController with the Storyboard ID (identifier) you gave it in the storyboard
+            let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
+            // Create a UItabBarItem (Remember that there is already one set by you in the storyboard)
+            vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+            // Add it to your tabBarController (it will sync well with the one you currently have)
+            tabBarViewController.viewControllers?.append(vc)
+        }
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    // Deleted functions below func application(). This app doesnt require them at this time.
 }
 
--- Whitehouse Petition (Parsing JSON)/Whitehouse Petition (Parsing JSON)/MainViewController.swift
+++ Whitehouse Petition (Parsing JSON)/Whitehouse Petition (Parsing JSON)/MainViewController.swift
@@ -22,15 +22,25 @@ class MainViewController: UITableViewController {
     
     func loadURL(){
         
-        // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        // Switch between tabBarItems
+        if navigationController?.tabBarItem.tag == 0{
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        }else{
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 // we're OK to parse!
                 parse(json: data)
+                return 
             }
         }
+        showError()
+        
     }
     
 
@@ -63,6 +73,16 @@ class MainViewController: UITableViewController {
         let vc = DetailViewController()
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    func showError(){
+        let ac = UIAlertController(title: "Loading error",
+                                   message: "There was a problem loading your data;\n please try again ",
+                                   preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel)
+        ac.addAction(action)
+        present(ac,animated: true)
     }
  
 }
