c7c86491a813fd784d46bfe08b897112b9574577
Ociel Lerma <oglerma@gmail.com>
2019-04-13 20:16:40 -0700

    This game will be only available in iPad.

---
+++ Swifty Words Game/Swifty Words Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swifty Words Game
+//
+//  Created by Ociel Lerma on 4/13/19.
+//  Copyright © 2019 Ociel Lerma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
