b549878bb07490dd211fc32cf5915c3964d795d4
Ociel Lerma <oglerma@gmail.com>
2019-04-26 08:53:42 -0700

    Made our GameViewController be called programatically throught appDelegate function. This will make it so that we don't have to use storyboard.... for now.

---
+++ HangmanApp/HangmanApp/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  HangmanApp
+//
+//  Created by Ociel Lerma on 4/26/19.
+//  Copyright © 2019 oglerma. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
