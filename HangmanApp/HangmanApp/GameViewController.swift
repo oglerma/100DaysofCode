7f98fd95044ca02196de4210217ef20ac12050ee
Ociel Lerma <olerma@ebay.com>
2019-04-29 16:27:45 -0700

    Added the remainder photo that was not included when images were created.

--- HangmanApp/HangmanApp/GameViewController.swift
+++ HangmanApp/HangmanApp/GameViewController.swift
@@ -14,6 +14,7 @@ class GameViewController: UIViewController {
     var correctWord_underscore = [String]()
     var correctWord_random = ""
     var allWords = [String]()
+    var letterBtns = [UIButton]()
     
     var answerUITextField: UITextField = {
         let answerUItxtField = UITextField()
