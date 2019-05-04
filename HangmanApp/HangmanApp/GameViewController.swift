9ffa069ddd1ba349c76ffde9dfbb810264cf6b07
Ociel Lerma <olerma@ebay.com>
2019-05-03 22:42:48 -0700

    Added the ability to start a new game if the user either wins or losses.

--- HangmanApp/HangmanApp/GameViewController.swift
+++ HangmanApp/HangmanApp/GameViewController.swift
@@ -15,14 +15,16 @@ class GameViewController: UIViewController {
     var correctWord_underscore = [String]()
     var correctWord_random = ""
     var allWords = [String]()
-    var letterBtns = [UIButton]()
     var emptyCorrectBoolArray = [Bool]()
     var updatedWord = ""
+    var numberOfTries = 1
+    var groups = [UIStackView]()
     
     var answerUITextField: UITextField = {
         let answerUItxtField = UITextField()
         answerUItxtField.isUserInteractionEnabled = false
-        answerUItxtField.backgroundColor = .green
+        answerUItxtField.layer.cornerRadius = 7
+        answerUItxtField.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
         answerUItxtField.textAlignment = .center
         return answerUItxtField
     }()
@@ -36,7 +38,7 @@ class GameViewController: UIViewController {
     
     var abcBtnView: UIView = {
         var abcBtn = UIView()
-
+        abcBtn.backgroundColor =  #colorLiteral(red: 0.1785905659, green: 0.1944591999, blue: 0.3133455813, alpha: 0.8722766285)
         return abcBtn
     }()
 
@@ -79,7 +81,7 @@ class GameViewController: UIViewController {
                                  size: .init(width: 0, height: 30))
         abcBtnView.anchor(top: answerUITextField.bottomAnchor,
                           leading: view.leadingAnchor,
-                          bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                          bottom: view.bottomAnchor,
                           trailing: view.trailingAnchor,
                           centerXaxis: nil,
                           centerYaxis: nil,
@@ -94,7 +96,6 @@ class GameViewController: UIViewController {
                     ["P", "Q","R", "S", "T"],
                     ["U", "V", "W","X", "Y"],
                     ["Z"]]
-        var groups = [UIStackView]()
         
         for i in list {
             let group = createButtons(named: i)
@@ -119,14 +120,16 @@ class GameViewController: UIViewController {
                          centerYaxis: nil,
                          padding: .init(top: 4, left: 4, bottom: 4, right: 4))
     }
+    
     func createButtons(named: [String]) -> [UIButton]{
         return named.map { letter in
             let button = UIButton()
             button.translatesAutoresizingMaskIntoConstraints = false
             button.layer.cornerRadius = 10
+            button.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
             button.addTarget(self, action: #selector(tappedABCbtns), for: .touchUpInside)
             button.setTitle(letter, for: .normal)
-            button.setTitleColor( .blue , for: .normal)
+            button.setTitleColor( .black , for: .normal)
             button.layer.borderWidth = 1
             return button
         }
@@ -134,7 +137,6 @@ class GameViewController: UIViewController {
     
     //MARK: LOGIC OF THE GAME
     @objc private func tappedABCbtns(_ sender: UIButton){
-        print("Inside button \(sender.titleLabel?.text ?? "inside")")
         guard let letter = sender.titleLabel?.text else {return}
         compareUserButtonWithAnswer(letter: letter)
         sender.setTitle("", for: .normal)
@@ -142,15 +144,19 @@ class GameViewController: UIViewController {
         sender.isUserInteractionEnabled = false
     }
     func compareUserButtonWithAnswer(letter: String){
-        for (index, char) in correctWord_random.enumerated() {
-            print("Index: \(index) and char \(char) inside of correctWord_random: \(correctWord_random)")
-            if letter == String(char) {
-                emptyCorrectBoolArray[index] = true
+        if correctWord_random.contains(letter){
+            for (index, char) in correctWord_random.enumerated() {
+                if letter == String(char) {
+                    emptyCorrectBoolArray[index] = true
+                }
             }
+            makeUpdatedArray()
+        } else {
+            updateHangmanImage()
         }
-        makeUpdatedArray()
+        
+
     }
-    
     func makeUpdatedArray(){
         for (index, value) in correctWord_random.enumerated() {
             print("Index: \(index) and value \(value) inside of correctWord_random: \(correctWord_random)")
@@ -161,9 +167,57 @@ class GameViewController: UIViewController {
             }
         }
         answerUITextField.text = updatedWord
+        
+        // Checking to see if there is any underscore in the answer
+        // if there is no underscore that means we won.
+        if !(answerUITextField.text?.contains("⏤ "))!{
+            youWon()
+        }
         updatedWord = ""
+        
     }
     
+    
+    func updateHangmanImage(){
+        numberOfTries += 1
+        hangmanImage.image = UIImage(named: "pic\(numberOfTries)")
+        
+        if numberOfTries == 6{
+            gameOver()
+        }
+    
+    }
+    
+    func gameOver(){
+        // Disable buttons
+        for btn in groups {
+            btn.isHidden = true
+        }
+        
+        let ac = UIAlertController(title: "Game Over", message: "You are dead!\n Want to play again?", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "OK", style: .default){
+            [weak self](action) in
+            self?.playAgain()
+        })
+        ac.addAction(UIAlertAction(title: "Nope", style: .default))
+        present(ac,animated: true)
+    }
+    
+    func youWon(){
+        for btn in groups {
+            btn.isHidden = true
+        }
+        
+        let ac = UIAlertController(title: "You Won!", message: "You are great!\n Want to play again?", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "OK", style: .default){
+            [weak self](action) in
+            self?.playAgain()
+        })
+        ac.addAction(UIAlertAction(title: "Nope", style: .default))
+        present(ac,animated: true)
+    }
+    
+
 
     
 
@@ -192,13 +246,21 @@ class GameViewController: UIViewController {
         correctWord_random = allWords.randomElement()?.uppercased() ?? "Turtle"
         createUnderscoreForGuessing()
         
-        // Zero out everything
-        
+    }
+    func playAgain(){
+        hangmanImage.image = UIImage(named: "pic1")
+        correctWord_underscore.removeAll()
+        emptyCorrectBoolArray.removeAll()
+        numberOfTries = 1
+        groups.removeAll()
+        answerUITextField.text = ""
+        startGame()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        navigationItem.title = "Hangman"
+        view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         performSelector(inBackground: #selector(loadImages), with: nil)
         loadWordList()
         addViews()
@@ -209,9 +271,3 @@ class GameViewController: UIViewController {
 
 }
 
-
-// TODO: LOGIC of the game. Compare the user input answer with each letter in the answer and display
-//       them in the view if they are correct.
-// TODO: Iterate through the next picture if the user gets it wrong.
-// TOOD: When user wins show a You WON screen with a button to start again. (Possibliy include a happy
-//       hangman gif. 😃
