62a4f972cc64c8bc4edd42b9549afe48bc25a156
Ociel Lerma <oglerma@gmail.com>
2019-05-04 17:35:57 -0700

    Adding ImagePickerController and the path to allow us to retrieve an image from apple photo app. We also have a Person cell that we created that can be modified. This is more of what the view of the actual app button will look like. Now in order to have a model that contain the information that will be dispalyed inside the Person cell, i also created a Person class that inherits from NSObject. This will allow us to store information and add information unto the Person Cell class throught actions performed in the Viewcontroller of they UICollectionViewController.

--- RecognizingFaces -UICollectionVC/RecognizingFaces -UICollectionVC/ViewController.swift
+++ RecognizingFaces -UICollectionVC/RecognizingFaces -UICollectionVC/ViewController.swift
@@ -8,20 +8,48 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var people = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Facial Recognition"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+    }
+    @objc func addNewPerson(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker,animated: true)
+        
     }
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 10
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {fatalError("Unable to deque a PersonCell")}
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {fatalError("Unable to dequeue a PersonCell")}
         
         return cell
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return}
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        if let jpegData = image.jpegData(compressionQuality: 0.8){
+            try? jpegData.write(to: imagePath)
+        }
+        let person = Person(name: "unknown", image: imageName)
+        people.append(person)
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
 }
 
