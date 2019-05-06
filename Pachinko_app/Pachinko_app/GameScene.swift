cc855049698a9efdb3189297eff1f4f2bb3fe12a
Ociel Lerma <olerma@ebay.com>
2019-05-06 08:32:07 -0700

    Using SKPhysicsBody() to add physics elements to our SKSpriteNodes.

--- Pachinko_app/Pachinko_app/GameScene.swift
+++ Pachinko_app/Pachinko_app/GameScene.swift
@@ -10,10 +10,23 @@ import SpriteKit
 
 
 class GameScene: SKScene {
+    
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
         addChild(background)
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 63, height: 64))
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        box.position = location
+        addChild(box)
+    }
 }
