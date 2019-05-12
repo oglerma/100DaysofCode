544c34259299cbe08579c3f347051f408cc5aa66
Ociel Lerma <oglerma@gmail.com>
2019-05-11 17:01:26 -0700

    Used JSONEncode() and JSONDecoder() to convert our people array into a data type and the latter to use it from our userdefualts when it launches again.

--- Names to face (Codable)/RecognizingFaces -UICollectionVC/ViewController.swift
+++ Names to face (Codable)/RecognizingFaces -UICollectionVC/ViewController.swift
@@ -16,6 +16,18 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         collectionView.backgroundColor = .black
         navigationItem.title = "Image Recognition"
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
+            }
+        }
     }
     @objc func addNewPerson(){
         let picker = UIImagePickerController()
@@ -51,6 +63,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         }
         let person = Person(name: "unknown", image: imageName)
         people.append(person)
+        save()
         collectionView.reloadData()
         dismiss(animated: true)
     }
@@ -96,10 +109,23 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
             [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else {return}
             person.name = newName
+            self?.save()
             self?.collectionView.reloadData()
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
+        
+    }
+    
+    func save(){
+        let jsonEncoder = JSONEncoder()
+        
+        if let savedData = try? jsonEncoder.encode(people){
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        }else{
+            print("Failed to save people")
+        }
         
     }
     
