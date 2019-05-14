03cdbaf1ab6266fb8a8790b1171f2ede53e92eca
Ociel Lerma <olerma@ebay.com>
2019-05-14 06:39:44 -0700

    Adding App that uses Camera and PhotoLibrary.

---
+++ Camera Capture App/Camera Capture App/CameraTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CameraTableViewController.swift
+//  Camera Capture App
+//
+//  Created by Lerma, Ociel(AWF) on 5/14/19.
+//  Copyright © 2019 oglerma. All rights reserved.
+//
+
+import UIKit
+
+class CameraTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = "hello"
+
+        return cell
+    }
+
+
+}
