1fac072d6f4f288aefc133d1ca178be2cc3924aa
Ociel Lerma <oglerma@gmail.com>
2019-05-13 18:26:41 -0700

    Loading saved information and creating bestScore property so that when the user beats its previous highest score then there is an alert that shows them that they have surpassed it.

--- Flag game with Auto Layout/Project2/ViewController.swift
+++ Flag game with Auto Layout/Project2/ViewController.swift
@@ -18,10 +18,14 @@ class ViewController: UIViewController {
     var score = 0
     var correctAnswer = 0
     var numberOfQuestionsAsked = 0
+   
+    var highScoreSaved = 0
+    var bestScore = 0
+    var beatBestScore = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadSavedInformation()
         // ADD BORDER RADIUS and COLOR
         var allButtons = [button1, button2, button3]
         for i in 0...allButtons.count - 1 {
@@ -38,19 +42,48 @@ class ViewController: UIViewController {
         askQuestion()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score", style: .done, target: self, action: #selector(showScore))
     }
+    func showUserBeatHighScore(){
+        let ac = UIAlertController(title: "You Beat you Previous High score of \(bestScore) by \(bestScore - highScoreSaved)",
+            message: "Keep going Champ!",
+            preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func loadSavedInformation(){
+        let defaults = UserDefaults.standard
+        if let savedHighScoreData = defaults.object(forKey: "highScoreSaved") as? Data{
+            let jsonDecoder = JSONDecoder()
+            do{
+                let currentScoreSaved = try jsonDecoder.decode(Int.self, from: savedHighScoreData)
+                // The Best score will be used to always keep the highest number we have ever gotten
+                if currentScoreSaved < bestScore {
+                    highScoreSaved = currentScoreSaved
+                }else {
+                    highScoreSaved = currentScoreSaved
+                    bestScore = currentScoreSaved
+                }
+            }catch{
+                print("Couldn't decode the highScoreSaved")
+            }
+        }
+    }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         numberOfQuestionsAsked += 1
 
+        print("This is best score \(bestScore)")
+        print("This is beatBestscore \(beatBestScore)")
         if numberOfQuestionsAsked >= 10{
+            if score > bestScore && !(bestScore <= 0) {
+                showUserBeatHighScore()
+            }
             showFinalAlert()
+            print("Showed final alert already")
         }else {
             showScoreAlert(buttonTagNumber: sender.tag)
         }
         print("This is the number of questions asked in buttonTapped = \(numberOfQuestionsAsked)")
-        
-        
-        
     }
     
     func showFinalAlert(){
@@ -108,8 +141,6 @@ class ViewController: UIViewController {
         present(showScoreAlert, animated: true)
     }
     
-    
-
     func addBorderRadius(addBorder borderBtn: UIButton?){
         borderBtn?.layer.borderWidth = 1
     }
@@ -117,4 +148,33 @@ class ViewController: UIViewController {
     func addCGColorToBorderWidth(borderColorBTN: UIButton?){
         borderColorBTN?.layer.borderColor = UIColor.lightGray.cgColor
     }
+    
+    func save(){
+        let jsonEncoder = JSONEncoder()
+        if let savedHighScore = try? jsonEncoder.encode(highScoreSaved){
+            let defaults = UserDefaults.standard
+            defaults.set(savedHighScore, forKey: "highScoreSaved")
+        } else {
+            print("Can't save high score")
+        }
+    }
+    /***************************************************
+     * SAVE() Explaniation
+     *          We need to convert the highScoreSaved integer to a Data type so that we can save
+     *          it in the UserDefaults. We could have made set the variable in a class or struct and
+     *          made it conform to the codable protocol but we can also just handle it right here with
+     *          JSONEncoder(). THis will encode our Integer, making it a Data type, and will be able
+     *          to give it a key so that we can access it wheverver we want after it is saved.
+     *          So we first Instantiate a JSONEndoer type.
+     *          Then we try to encode the highScoreSaved Integer, conveniently with a try statment.
+     *          if it converts it to Data type then we set it to our user Defaults. We can easilty just say
+     *          Userdefault.standard.set(DATA Type in this case highScoreSaved, Key that you want to give it)
+     *          Now it is saved in user defaults. To retrieve this information when we relaunch the app, we need to
+     *          basically go to our USerdefaults and check to see if the highScoreSaved Data that we saved is there.
+     *          since we gave it a key word we can check if it is there and assign it back the the property that it
+     *          belongs to. Since it in the begining that it does this then everything will be updated as it previosly was
+     *          before we shut the app.
+     ***************************************************/
+
+    
 }
