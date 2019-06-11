6da2cb5050933cdb6623c5b0fb5796a7234357d9
Ociel Lerma <oglerma@gmail.com>
2019-06-11 07:45:00 -0700

    Refactored code and use the built in render method from the delegate to alter the anchor of the item i am placing in our ARSCNView.

--- ARDicee/ARDicee/ViewController.swift
+++ ARDicee/ARDicee/ViewController.swift
@@ -12,17 +12,78 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
-    
+    //MARK- Properites
+    @IBOutlet var sceneView: ARSCNView! = {
+       var scnView = ARSCNView()
+        scnView.showsStatistics = true
+        scnView.autoenablesDefaultLighting = true
+        scnView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        return scnView
+    }()
+    /***************************************************
+     * ViewDidLoad
+     ***************************************************/
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
+        createScene()
+    }
+    
+    /***************************************************
+     * CreateScene
+     ***************************************************/
+    func createScene(){
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
+            diceNode.position = SCNVector3(0, 0, -0.1)
+            // Set the scene to the view
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+    }
+    
+    /***************************************************
+     * ViewWillAppear
+     ***************************************************/
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    }
+    
+    /***************************************************
+     * ViewWillDisappear
+     ***************************************************/
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+
+    /***************************************************
+     * Renderer
+     ***************************************************/
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            let planeAnchor = anchor as! ARPlaneAnchor
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            node.addChildNode(planeNode)
+        }else {
+            return
+        }
+    }
+
+}
+
 ////        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
 //        let sphere = SCNSphere(radius: 0.2)
 //        let material = SCNMaterial()
@@ -34,59 +95,3 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        node.geometry = sphere
 //
 //        sceneView.scene.rootNode.addChildNode(node)
-        sceneView.autoenablesDefaultLighting = true
-        
-        
-        // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
-            diceNode.position = SCNVector3(0, 0, -0.1)
-            // Set the scene to the view
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        }
-
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
-}
