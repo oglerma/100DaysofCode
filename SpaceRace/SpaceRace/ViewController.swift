5a78a9ca72e18951301c3d4abb0d921be5407f82
Ociel Lerma <olerma@ebay.com>
2019-06-18 14:44:59 -0700

    Adding a new Mobile App game. This app will consist on having a spacecraft that dodges object. It will incorporate the timer to keep track on how long we live. The longer we live the more points we make.

---
+++ SpaceRace/SpaceRace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceRace
+//
+//  Created by Lerma, Ociel(AWF) on 6/18/19.
+//  Copyright © 2019 oglerma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
