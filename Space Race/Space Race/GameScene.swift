ed6c65a2ee317c3b7351b93545cfd50f5ec487da
Ociel Lerma <olerma@ebay.com>
2019-06-18 14:56:37 -0700

    Added a new Project called Space Race. Here we will learn how to build an app quickly and use serval methods and properites provided by spritekit.

---
+++ Space Race/Space Race/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Space Race
+//
+//  Created by Lerma, Ociel(AWF) on 6/18/19.
+//  Copyright © 2019 oglerma. All rights reserved.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+
+    override func didMove(to view: SKView) {
+  
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+
+    }
+}
