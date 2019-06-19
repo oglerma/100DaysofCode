b7703159b8ae3782bdc6b4f4911e0357a0953f18
Ociel Lerma <olerma@ebay.com>
2019-06-19 14:44:41 -0700

    Shows how to use the print statement more effectively. However, there is breakpoints that we can use and give the break point conditions. Also we have asserts that can assert which see if the condition that you are trying to obtain is true or false. If it is true nothing happens but if it is false the app crashes and shows you were it crashed with whatever message you added in the assert.

--- Debugging/Debugging/ViewController.swift
+++ Debugging/Debugging/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // USING PRINT STATEMTNS
+        print(1,2,3,4,5, separator: "-")
+        // PRINTS (1-2-3-4-5)
+        print("Another message", terminator: "\n")
+        // PRINTS ANOTHER MESSAGE with a Line BREAK
+        print("Some message", terminator: "")
+        // DOES NOT PRINT WITH LINE BREAK
+        
+        
+        assert(1 == 1, "Math failure!")
+        
+        for i in 1...100 {
+            print("GOT NUMBER: \(i)")
+        }
+        
     }
 
 
