6c4f8747f330a315d2eab5ba1717a26d2ff475d7
Ociel Lerma <olerma@ebay.com>
2019-06-25 15:27:00 -0700

    Added background images, duck targets, several constant enums to prevent mistyping, and started handling movement of the water images and the duck images. the duck images can now move from left to right throughout the entire X axis and the water moves sligtly back and forth and up and down.

--- ShootingGame APP/ShootingGame APP/Controller/GameScene.swift
+++ ShootingGame APP/ShootingGame APP/Controller/GameScene.swift
@@ -8,20 +8,137 @@
 
 import SpriteKit
 
-
 class GameScene: SKScene, SKPhysicsContactDelegate {
-
+    
+    // MARK: - Background Images
     var curtainImg: SKSpriteNode = {
-       let curtain = SKSpriteNode(imageNamed: "curtains")
+        let curtain = SKSpriteNode(imageNamed: "curtains")
         curtain.position = CGPoint(x: 512, y: 384)
-        curtain.zPosition = -1
-//        curtain.blendMode = .replace
+        curtain.zPosition = Rows.five.value
         return curtain
     }()
     
+    var frontWater: SKSpriteNode = {
+        let fwater = SKSpriteNode(imageNamed: "water-fg")
+        fwater.zPosition = Rows.four.value
+        fwater.position = CGPoint(x: 512, y: 190)
+        return fwater
+    }()
+    
+    var backWater: SKSpriteNode = {
+        let bwater = SKSpriteNode(imageNamed: "water-bg")
+        bwater.position = CGPoint(x: 512, y: 200)
+        bwater.zPosition = Rows.two.value
+        return bwater
+    }()
+    
+    var grass: SKSpriteNode = {
+        let gr = SKSpriteNode(imageNamed: "grass-trees")
+        gr.position = CGPoint(x: 512, y: 384)
+        gr.zPosition = Rows.one.value
+        return gr
+    }()
+    
+    // MARK: - Duck Targets
+    var topDuck: SKSpriteNode = {
+        let duck = SKSpriteNode(imageNamed: "target1")
+        duck.position = CGPoint(x: 300, y: 450)
+        duck.zPosition = Rows.zero.value
+        return duck
+    }()
+    
+    var middleDuck: SKSpriteNode = {
+        let duck = SKSpriteNode(imageNamed: "target3")
+        duck.position = CGPoint(x: 0, y: Location.middle.value)
+        duck.zPosition = Rows.two.value
+        return duck
+    }()
+    
+    var bottomDuck: SKSpriteNode = {
+        let duck = SKSpriteNode(imageNamed: "target2")
+        duck.position = CGPoint(x: 0, y: Location.bottom.value)
+        duck.zPosition = Rows.four.value
+        return duck
+    }()
+    
+
+    // MARK: - GAME LOGIC BEGINS
+    
     override func didMove(to view: SKView) {
+        // SET sizes
+        curtainImg.size = view.frame.size
+        frontWater.size = CGSize(width: view.frame.width, height: frontWater.frame.height)
+        backWater.size  = CGSize(width: view.frame.width, height: backWater.frame.height)
+        grass.size      = CGSize(width: view.frame.width, height: grass.frame.height)
+        
         addChild(curtainImg)
+        addChild(bottomDuck)
+        addChild(middleDuck)
+        addChild(frontWater)
+        addChild(backWater)
+        addChild(grass)
+        addChild(topDuck)
+
+        
+        moveDucks()
+        moveWater()
+        
+        
+        
+    }
+    
+    // MARK: - Handling game logic
+    /***************************************************
+     * moveWater gives the effect of the water images moving back and forth.
+     ***************************************************/
+    func moveWater(){
+        let moveRight = SKAction.move(by: CGVector(dx: 25, dy: 7), duration: 0.5)
+        let moveLeft = SKAction.move(by: CGVector(dx: -25, dy: -7), duration: 0.5)
+        
+        let sequence = SKAction.sequence([moveRight, moveLeft])
+        let sequence2 = SKAction.sequence([moveLeft, moveRight])
+        
+        let repeatForever = SKAction.repeatForever(sequence)
+        let repeatForever2 = SKAction.repeatForever(sequence2)
+        
+        frontWater.run(repeatForever)
+        backWater.run(repeatForever2)
+    }
+    
+    
+    
+    /***************************************************
+     * Moves duck left and right
+     ***************************************************/
+    func moveDucks(){
+        let moveRight = SKAction.move(by: CGVector(dx: Move.right.value,
+                                                   dy: Move.up.value), duration: 3)
+        
+        let moveLeft = SKAction.move(by: CGVector(dx: Move.left.value,
+                                                  dy: Move.down.value), duration: 3)
+        
+        let sequence = SKAction.sequence([moveRight,moveLeft])
+        let repeatForever = SKAction.repeatForever(sequence)
+        middleDuck.run(repeatForever)
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        let middleDuckXPosition = Int(middleDuck.position.x)
+        
+        
+        if middleDuckXPosition > 1000 || middleDuckXPosition < 20 {
+            middleDuck.xScale = middleDuck.xScale * -1
+            print("First")
+            print(middleDuck.xScale)
+            print(middleDuck.position.x)
+        }
     }
     
 
+
 }
+
+
+
+
---
+++ ShootingGame APP/ShootingGame APP/Model/Constants.swift
@@ -0,0 +1,89 @@
+//
+//  Constants.swift
+//  ShootingGame APP
+//
+//  Created by Lerma, Ociel(AWF) on 6/25/19.
+//  Copyright © 2019 oglerma. All rights reserved.
+//
+
+import UIKit
+/***************************************************
+ * Move.left.value will return the value amount by
+ * which we want to move our nodes in the X and Y axis.
+ ***************************************************/
+enum Move {
+    case left
+    case right
+    case up
+    case down
+    
+    var value: Int {
+        switch self {
+        case .left:
+            return -1024
+        case .right:
+            return 1024
+        case .up:
+            return 0
+        case .down:
+            return 0
+        }
+    }
+}
+
+
+/***************************************************
+ * Rows.zero.value will be used to reference on what zPosition
+ * is each node currently displaying on the screen. 
+ ***************************************************/
+enum Rows {
+    case zero
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    
+    var value: CGFloat {
+        switch self {
+        case .zero:
+            return 0
+        case .one:
+            return 1
+        case .two:
+            return 2
+        case .three:
+            return 3
+        case .four:
+            return 4
+        case .five:
+            return 5
+        case .six:
+            return 6
+        }
+}
+}
+
+/***************************************************
+ * Location.top.value return the value associated with top DUCK.
+ ***************************************************/
+
+enum Location {
+    case top
+    case middle
+    case bottom
+    
+    var value: Int {
+        switch self {
+        case .top:
+            return 400
+        case .middle:
+            return 320
+        case .bottom:
+            return 140
+        }
+    }
+}
+
+
