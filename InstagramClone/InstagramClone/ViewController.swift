83fdf46f500f553ec9a8e81ebcb5382b9d0fb196
Ociel Lerma <oglerma@gmail.com>
2019-08-25 17:06:48 -0500

    Added Target actions for our email, username, password textfields and the sign up button. Now the user will see the Sign up button not enabled until the textfield are able to pass the required criteeria that we programmend. Once the criteria has been passed, the sign up button will be enabled and you will be able to sign up. Also used AUTH.auth.createUser to create a new user and save the information in our database.

--- InstagramClone/InstagramClone/ViewController.swift
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -22,15 +23,31 @@ class ViewController: UIViewController {
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
+    @objc func handleTextInputChange(){
+        let isFormValid = emailTextField.text?.count ?? 0 > 8 &&
+            usernameTextField.text?.count ?? 0 > 0 &&
+            passwordTextField.text?.count ?? 0 > 0
+        
+        if isFormValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 164, blue: 237)
+        } else {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        }
+        
+    }
     let usernameTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Username"
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
@@ -41,6 +58,7 @@ class ViewController: UIViewController {
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
@@ -51,8 +69,26 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.isEnabled = false
         return button
     }()
+    
+    @objc func handleSignUp(){
+        guard let email    = emailTextField.text   , email.count > 8    else {return}
+        guard let username = usernameTextField.text, username.count > 0 else {return}
+        guard let password = passwordTextField.text, password.count > 0 else {return }
+
+        
+        Auth.auth().createUser(withEmail: email, password: password, completion: {
+            (user, error) in
+            if let err = error {
+                print("Failed to create user: ", err)
+                return
+            }
+            print("Successfully created user: ", user?.user.uid ?? "")
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(plusPhotoButton)
