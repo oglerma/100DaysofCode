079c3d97958bde15bbe1cdb1102a038a99c48753
Ociel Lerma <oglerma@gmail.com>
2019-08-27 12:45:10 -0500

    Added ImagePicker to go through the photoLibrary and allow the user to select an image. Used the Firebase Database and Authentication to create a user and save it in the Firebase database.

--- InstagramClone/InstagramClone/ViewController.swift
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -9,14 +9,45 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "plus_photo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
     
+    /***************************************************
+     * Display image picker
+     ***************************************************/
+    @objc func handlePlusPhoto(){
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+            
+        }else if let originalImage = info[.originalImage] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+
+
+    }
+
     let emailTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Email"
@@ -27,20 +58,7 @@ class ViewController: UIViewController {
         return tf
     }()
     
-    @objc func handleTextInputChange(){
-        let isFormValid = emailTextField.text?.count ?? 0 > 8 &&
-            usernameTextField.text?.count ?? 0 > 0 &&
-            passwordTextField.text?.count ?? 0 > 0
-        
-        if isFormValid {
-            signUpButton.isEnabled = true
-            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 164, blue: 237)
-        } else {
-            signUpButton.isEnabled = false
-            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
-        }
-        
-    }
+
     let usernameTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Username"
@@ -74,25 +92,57 @@ class ViewController: UIViewController {
         return button
     }()
     
+    /***************************************************
+     * Action
+     ***************************************************/
+
+    @objc func handleTextInputChange(){
+        let isFormValid = emailTextField.text?.count ?? 0 > 8 &&
+            usernameTextField.text?.count ?? 0 > 0 &&
+            passwordTextField.text?.count ?? 0 > 0
+        
+        if isFormValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 164, blue: 237)
+        } else {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        }
+        
+    }
     @objc func handleSignUp(){
         guard let email    = emailTextField.text   , email.count > 8    else {return}
         guard let username = usernameTextField.text, username.count > 0 else {return}
         guard let password = passwordTextField.text, password.count > 0 else {return }
 
         
+      
         Auth.auth().createUser(withEmail: email, password: password, completion: {
             (user, error) in
-            if let err = error {
-                print("Failed to create user: ", err)
-                return
-            }
-            print("Successfully created user: ", user?.user.uid ?? "")
+
+            guard let uid = user?.user.uid else {return}
+            let usernameValues = ["username", username]
+            let values = [uid: usernameValues]
+            // Save in the database
+            Database.database().reference().updateChildValues(values, withCompletionBlock: { (err, ref) in
+                if let err = error {
+                    print("Failed to create user: ", err)
+                    return
+                }
+                print("Successfully created user: ", user?.user.uid ?? "")
+            })
         })
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(plusPhotoButton)
-        plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBotton: 0, paddingRight: 0, width: 140, height: 140)
+        plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil,
+                               bottom: nil, right: nil,
+                               paddingTop: 40, paddingLeft: 0,
+                               paddingBotton: 0, paddingRight: 0,
+                               width: 140, height: 140)
+        
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         setupInputFields()
         
